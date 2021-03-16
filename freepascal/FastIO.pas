06f9783c2b086ae33e35860baca7a45a04b11fb3
BenGrasset <operator97@gmail.com>
2021-03-16 00:21:41 -0400

    Add a Free Pascal "optimized" program, as well as a utility unit written for it specifically for this benchmark

---
+++ freepascal/FastIO.pas
@@ -0,0 +1,61 @@
+unit FastIO;
+
+{$mode Delphi}{$H+}{$J-}
+{$ImplicitExceptions Off}
+{$RangeChecks Off}
+{$COperators On}
+
+interface
+
+type
+  FileFunc = procedure(var T: TextRec);
+  PTextRec = ^TextRec;
+
+procedure FastLowerCase(PS: PChar); {$ifndef DEBUG}inline;{$endif}
+procedure FastReadStr(PT: PTextRec; var S: ShortString); {$ifndef DEBUG}inline;{$endif}
+function FastCheckEOF(PT: PTextRec): Boolean; {$ifndef DEBUG}inline;{$endif}
+
+implementation
+
+procedure FastLowerCase(PS: PChar);
+var
+  I: PtrInt;
+  P: PChar absolute PS;
+begin
+  for I := 1 to Ord((P - 1)^) do begin
+    if P^ in ['A'..'Z'] then
+      P^ := Char(Byte(P^) + 32);
+    Inc(P);
+  end;
+end;
+
+procedure FastReadStr(PT: PTextRec; var S: ShortString);
+var
+  C: Char;
+  P: PChar;
+begin
+  P := @S[1];
+  S[0] := #0;
+  while True do begin
+    if PT^.BufPos >= PT^.BufEnd then
+      FileFunc(PT^.InOutFunc)(PT^);
+    C := PT^.BufPtr^[PT^.BufPos];
+    Inc(PT^.BufPos);
+    if C in [#10, #32] then Break;
+    P^ := C;
+    Inc(P);
+    Inc(S[0]);
+  end;
+end;
+
+function FastCheckEOF(PT: PTextRec): Boolean;
+begin
+  if PT^.BufPos >= PT^.BufEnd then begin
+    FileFunc(PT^.InOutFunc)(PT^);
+    if PT^.BufPos >= PT^.BufEnd then
+      Exit(True);
+  end;
+  Result := CtrlZMarksEOF and (PT^.BufPtr^[PT^.BufPos] = #26);
+end;
+
+end.
---
+++ freepascal/Optimized.pas
@@ -0,0 +1,66 @@
+program Optimized;
+
+// Delphi-syntax mode allows for generics without using the 'generic' and 'specialize' keywords.
+{$mode Delphi}{$H+}{$J-}
+// By default Free Pascal generates exception handling code to pad certain things,
+// which for our extremely performance-focused purposes here is not desirable.
+{$ImplicitExceptions Off}
+// Suppresses a spurious warning about a static array buffer we use below not being "initialized",
+// which is in no way necessary in this particular context.
+{$Warn 5058 Off}
+
+uses
+  // A few speed-optimized IO helper routines that do less "safety checks" internally
+  // than the defaults provided by the language implementation.
+  FastIO,
+  // Units from an excellent third-party generics library called 'LGenerics':
+  // https://github.com/avk959/LGenerics
+  lgArrayHelpers,
+  lgHashMultiSet;
+
+type
+  // Some type aliases for the sake of convenience.
+  TStrCounter = TGLiteHashMultiSetLP<ShortString, ShortString>.TMultiSet;
+  TStrEntry = TStrCounter.TEntry;
+  THelper = TGRegularArrayHelper<TStrEntry>;
+
+  // We'll use this to sort our final array of string / integer pairs below.
+  function CompareEntries(const L, R: TStrEntry): Boolean; {$ifndef DEBUG}inline;{$endif}
+  begin
+    Result := L.Count > R.Count;
+  end;
+
+var
+  InBuf: array[0..65534] of Byte;
+  PIn: PTextRec;
+  POut: PText;
+  S: ShortString = '';
+  SC: TStrCounter;
+  E: TStrEntry;
+  EA: TStrCounter.TEntryArray;
+
+begin
+  // `Input` and `Output` are `threadvars`, so if we don't take pointers directly to them the
+  // compiler will continuously generate code that goes and "gets" them anytime we do anything
+  // IO related that implicitly involves them.
+  PIn := @Input;
+  SetTextBuf(Text(PIn^), InBuf);
+  POut := @Output;
+  // All we have to do is keep adding the strings to the multiset, as it automatically generates
+  // the counts we want in the process.
+  while not FastCheckEOF(PIn) do begin
+    FastReadStr(PIn, S);
+    if S[0] = #0 then Continue;
+    FastLowercase(@S[1]);
+    SC.Add(S);
+  end;
+  // Get a contiguous array of all the string / count pairs.
+  EA := SC.ToEntryArray();
+  // Sort the array.
+  THelper.Sort(EA, ComparePairs);
+  // Display the array.
+  for E in EA do with E do
+    // Doing it this way instead of using `WriteLn` will force Unix newlines even on Windows, so as
+    // to guarantee 'output.txt' matches with the original in terms of file size on all platforms.
+    Write(POut^, Key, ' ', Count, #10);
+end.
