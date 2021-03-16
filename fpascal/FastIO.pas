6de28be7d6f90209e86e5416f4ec511881771633
Akira13641 <operator97@gmail.com>
2021-03-16 15:41:56 -0400

    Revert last change
    Also get rid of now-unused functions

--- fpascal/FastIO.pas
+++ fpascal/FastIO.pas
@@ -10,24 +10,10 @@ type
   FileFunc = procedure(var T: TextRec);
   PTextRec = ^TextRec;
 
-procedure FastLowerCase(PS: PChar); inline;
 procedure FastReadLowerStr(PT: PTextRec; var S: ShortString); inline;
-procedure FastSetTextBuf(PT: PTextRec; var Buf; const Size: PtrInt); inline;
 function FastCheckEOF(PT: PTextRec): Boolean; inline;
 
 implementation
-
-procedure FastLowerCase(PS: PChar);
-var
-  I: PtrInt;
-  P: PChar absolute PS;
-begin
-  for I := 1 to Ord((P - 1)^) do begin
-    if P^ in ['A'..'Z'] then
-      P^ := Char(Byte(P^) + 32);
-    Inc(P);
-  end;
-end;
 
 procedure FastReadLowerStr(PT: PTextRec; var S: ShortString);
 var
@@ -40,6 +26,7 @@ begin
     if PT^.BufPos >= PT^.BufEnd then
       FileFunc(PT^.InOutFunc)(PT^);
     C := PT^.BufPtr^[PT^.BufPos];
+    Inc(PT^.BufPos);
     case C of
       #10, #32: Break;
       #65..#90: C := Char(Byte(C) or Byte(Byte(True) shl 5));
@@ -47,17 +34,6 @@ begin
     P^ := C;
     Inc(P);
     Inc(S[0]);
-    Inc(PT^.BufPos);
-  end;
-end;
-
-procedure FastSetTextBuf(PT: PTextRec; var Buf; const Size: PtrInt);
-begin
-  with PT^ do begin
-    BufPtr := @Buf;
-    BufSize := Size;
-    BufPos := 0;
-    BufEnd := 0;
   end;
 end;
 
@@ -68,7 +44,10 @@ begin
     if PT^.BufPos >= PT^.BufEnd then
       Exit(True);
   end;
-  Result := CtrlZMarksEOF and (PT^.BufPtr^[PT^.BufPos] = #26);
+  {$ifndef Linux}
+    // The next line is always false on Linux, so don't bother with it there.
+    Result := CtrlZMarksEOF and (PT^.BufPtr^[PT^.BufPos] = #26);
+  {$endif}
 end;
 
 end.
